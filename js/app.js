1cb2f0c9dfe9a924a56d3f5cc6dde9948f019d20
M Holmquist <mai.holmquist@gmail.com>
2019-09-09 18:07:28 -0700

    commting work

--- js/app.js
+++ js/app.js
@@ -1,2 +1,43 @@
-const storeName = '';
+const storeName = 'Farm Shop Grocery';
 document.title = `${storeName} Grocery Store`;
+console.log(`what is in document:`, document.all);
+console.log(document.getElementsByTagName('p'));
+
+let h1_OfHeader = document.getElementById('mainIntro');
+// console.log(h1_OfHeader);
+h1_OfHeader.innerHTML = 'Welcome';
+
+ const getAllNavListClasses = document.querySelectorAll('.nav_list_item');
+console.log('headers:',  getAllNavListClasses);
+
+const getIconCredit = document.querySelector('#iconsCredit');
+const testDifference = document.getElementById('iconsCredit');
+
+console.log(getAllNavListClasses);
+console.log(testDifference);
+
+const example = document.querySelector('.nav_list_item');
+console.log(example);
+
+const getClasses = document.getElementsByClassName('nav_list_item');
+console.log(getClasses);
+
+const imageExample = document.querySelector('img[alt="cherry icon"]');
+console.log(imageExample);
+
+
+const magicButton = document.getElementById('magic');
+console.log(magic);
+
+magicButton.addEventListener('mouseleave', () => {
+    magicButton.className ='';
+    console.log('leaving:', magicButton);
+});
+
+document.addEventListener('keydown', () => {
+    console.log(event.keyCode);
+});
+
+magicButton.addEventListener('click', () => {
+     document.getElementById('colorMe').style.backgroundColor='#000';
+});
